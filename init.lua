412c0cfb974ef6604ed12455c95a1d2f1f9b0514
Niklas Claesson <nicke.claesson@gmail.com>
2019-09-04 20:10:59 +0200

    wip

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- Tested with wireshark 2.8
+
 u2f_proto = Proto("u2f", "U2F")
 
 local cids = {[0xffffffff] = "BROADCAST"}
@@ -21,9 +23,27 @@ local f_init_cid = ProtoField.uint32("u2f.init.cid", "CID", base.HEX)
 local f_init_version = ProtoField.uint8("u2f.init.version", "VERSION", base.DEC)
 local f_init_cap = ProtoField.uint8("u2f.init.capabilities", "CAP", base.HEX)
 
+local u2f_inss = {
+        [1] = "U2F_REGISTER",
+        [2] = "U2F_AUTHENTICATE",
+        [3] = "U2F_VERSION",
+        [0x40] = "U2F_VENDOR0",
+        [0x41] = "U2F_VENDOR1",
+}
+local f_msg_ins = ProtoField.uint8("u2f.msg.ins", "INS", base.HEX, u2f_inss)
+local u2f_p1s = {
+        [3] = "enforce-user-presence-and-sign",
+        [7] = "check-only",
+        [8] = "dont-enforce-user-presence-and-sign",
+}
+local f_msg_p1 = ProtoField.uint8("u2f.msg.p1", "P1", base.HEX, u2f_p1s)
+
 local bytes_left = 0
 
-u2f_proto.fields = { f_cid, f_cmd, f_seq, f_bcnt, f_payload, f_init_nonce, f_init_cid, f_init_version, f_init_cap}
+u2f_proto.fields = {
+        f_cid, f_cmd, f_seq, f_bcnt, f_payload, f_init_nonce, f_init_cid, f_init_version,
+        f_init_cap, f_msg_ins, f_msg_p1
+}
 
 local cbor_proto = Dissector.get("cbor")
 
@@ -47,6 +67,11 @@ function u2f_proto.dissector(buffer, pinfo, tree)
                         pinfo.cols['info'] = "U2FHID ECHO"
                 elseif cmd == bit32.bor(0x80, 0x03) then
                         pinfo.cols['info'] = "U2FHID MSG"
+                        -- Responses do not have these fields. But we cannot track conversations
+                        -- in a lua implementation of this protocol
+                        local msgtree = subtree:add("MSG"), buffer(7, math.min(bcnt, 64-7))
+                        msgtree:add(f_msg_ins, buffer(8,1))
+                        msgtree:add(f_msg_p1, buffer(9, 1))
                 elseif cmd == bit32.bor(0x80, 0x04) then
                         pinfo.cols['info'] = "U2FHID LOCK"
                 elseif cmd == bit32.bor(0x80, 0x06) then
